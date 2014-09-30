c6a1334a513d90b211ee3078fdc360fd93ba809d
Nicholas Hare <oharemarketing@gmail.com>
2014-09-30 20:59:58 +0000

    @nhare @jmanock

--- dojo--2014-09-30.js
+++ dojo--2014-09-30.js
@@ -15,14 +15,38 @@
  */
  
  function wordVal (word){
-     if(word == "zero"){
+    if(word == "zero"){
          word = 0;
-     }
+    }
     if ( word == "one"){
         word = 1;
     } 
     if (word == "two"){
         word = 2;
+    }
+    if(word == "three"){
+        word = 3;
+    }
+    if (word == "four"){
+        word = 4;
+    }
+    if (word == "five"){
+        word = 5;
+    }
+    if (word == "six"){
+        word = 6;
+    }
+    if (word == "seven"){
+        word = 7;
+    }
+    if (word == "eight"){
+        word = 8;
+    }
+    if (word == "nine"){
+        word = 9;
+    }
+    if (word == "ten"){
+        word = 10;
     }
     return word;
 }
@@ -32,8 +56,18 @@ function test_wordVal(a, b){
         wordVal(a) === b);
 }
 
+test_wordVal('zero', 0);
 test_wordVal('one', 1);
- 
+test_wordVal('two', 2);
+test_wordVal('three', 3);
+test_wordVal('four', 4);
+test_wordVal('five', 5);
+test_wordVal('six', 6);
+test_wordVal('seven',7);
+test_wordVal('eight',8);
+test_wordVal('nine',9);
+test_wordVal('ten',10);
+
 function subtract(a,b){
      
     a = wordVal(a);
@@ -45,9 +79,13 @@ function subtract(a,b){
 }
  
 function test_subtract(a, b, c){
-     console.log('it should subtract ' + a + 'and ' + b + ':',
+     console.log('it should subtract ' + a + ' and ' + b + ':',
      subtract(a,b) === c);
 }
  
  test_subtract("one", "one", 0);
- 
+ test_subtract("two ", "one", 1);
+ test_subtract("two", "two", 0);
+ test_subtract("three", "one", 2);
+ test_subtract("three", "two", 1);
+ test_subtract("three", "three", 0);
