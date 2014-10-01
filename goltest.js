06983e7c9c789778ea25acfda2288db777212c3e
Nicholas Hare <oharemarketing@gmail.com>
2014-10-01 11:27:59 -0400

    goltest

---
+++ goltest.js
@@ -0,0 +1,14 @@
+
+var assert = require('assert');
+
+function conway{cell, neighbors){
+}
+
+var cell = false,
+    neighbors = [
+      false, false, false, false,
+      false, false, false, false,
+    ];
+
+assert.equal(
+    conway(cell, nieghbors) === false), || console.log('Pass');
