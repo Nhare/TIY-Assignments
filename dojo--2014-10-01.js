d7670a5838c1ac7888e20b37c94a43b008beadbc
Nicholas Hare <oharemarketing@gmail.com>
2014-10-01 17:27:11 -0400

    blarg

--- dojo--2014-10-01.js
+++ dojo--2014-10-01.js
@@ -1,8 +1,21 @@
-var input = "1.00";
-var nombre = "zero"
 
-if (input.charAt(0) == 1){
-  nombre = "one";
+var cents1 = 0;
+var cents2 = 0;
+var a = 0;
+var input = function(a){
+//  cents1 = a.charAt(3);
+//  cents2 = a.charAt(4);
+  if (a.charAt(0) == 1){
+    return "one";
+  }
 }
 
-console.log("You wrote a check for " + nombre + " dollar(s) : $1.00");
+console.log(a);
+
+//console.log('You wrote a check for ' + a + ' dollar(s) and ' + cents1 + cents2 + '/100 cents. : $1.00',
+console.log("I want to input 1.00 and get one back.");
+  input("1.00") === "one");
+
+//console.log('You wrote a check for ' + a + ' dollar(s) and ' + cents1 + cents2 + '/100 cents. : $1.56',
+
+  input("1.56"));
