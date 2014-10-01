deda07effcae805ff22b659e6ddaac1f6ddeefae
Nicholas Hare <oharemarketing@gmail.com>
2014-10-01 12:06:02 -0400

    checkpoint

--- goltest.js
+++ goltest.js
@@ -1,8 +1,44 @@
 
 var assert = require('assert');
 
-function conway{cell, neighbors){
+function conway(cell, neighbors){
+  if (liveNeighbors >= 2)
+      return true;
 }
+
+function numberOfLiveNeighbors(neighbors){
+   //What do I do with neighbors?
+
+    if (neighbors[0] === true){
+        return 1;
+    }
+    if (neighbors[1] === true){
+        return 1;
+    }
+    
+        //With each neighbor...
+      // If neighbor is TRUE
+        //add 1 live neighbor
+      // Return live neighbors
+}
+
+function test_numberOfLiveNeighbors(neighbors, expected, successMessage){
+      var actual = numberOfLife Neighbors(neighbors);
+
+      assert(actual === expected) || console.log(message);
+}
+
+test_numberOfLiveNeighbors([
+      false, false, false,
+      false,        false,
+      false, false, false
+  ]), 0, 'no live neighbors');
+
+test_numberOfLiveNeighbors([
+      false, true, false,
+      false,        false,
+      false, false, false
+  ], 1, 'one live neighbor');
 
 var cell = false,
     neighbors = [
@@ -10,5 +46,36 @@ var cell = false,
       false, false, false, false,
     ];
 
-assert.equal(
-    conway(cell, nieghbors) === false), || console.log('Pass');
+assert.equal(conway(cell, neighbors), false,
+    'an empty board should remain empty'
+) || console.log('Pass');
+
+neighbors = [
+false, false,  false,
+false,        false,
+false, false, false
+];
+
+assert.equal(conway(true, neighbors), false,
+    'a cell with no neighbors dies'
+) || console.log('Pass');
+
+neighbors = [
+    false, true,  false,
+    false,        false,
+    false, false, false
+];
+
+assert.equal(conway(true, neighbors), false,
+    'a cell with only one neighbor dies'
+) || console.log('Pass');
+
+neighbors = [
+    true,  true,  false,
+    false,        false,
+    false, false, false
+];
+
+assert.equal(conway(true, neighbors), false,
+    'a cell with 2 to 3 living neighbors survives'
+) || console.log('Pass');
