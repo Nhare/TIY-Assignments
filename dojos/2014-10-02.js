ee899cc53ec30680f88966cb9b01e30edcff32ee
Nicholas Hare <oharemarketing@gmail.com>
2014-10-04 19:29:32 -0400

    functionchaos

--- dojos/2014-10-02.js
+++ dojos/2014-10-02.js
@@ -57,6 +57,9 @@
 //     console.log(success);
 // }
 
+// var tick = [];
+// tick.push(newState);
+// console.log(tick);
 
 
 board = [
@@ -65,77 +68,87 @@ board = [
   [false, false, false],
 ]
 
-// var tick = [];
-// tick.push(newState);
-// console.log(tick);
+//var newTick = [];
+//function tick(board){
 
+//cycles through board for position
+var cellBool = [];
+var cellLoc = [];
 for (i = 0; i < board.length; i++){
     var b = board[i];
     for(j = 0; j < b.length; j++){
         var pos = b[j];
+        var x = i;
+        var y = j;
+        cellBool.push(pos);
+        cellLoc.push(x, y);
+        //takes position and checks for neighbors (couldn't figure out my other neighbor code)
+        function neighborOf(x, y) {
+            if (x === 0 && y === 0) {
+              neighborsAre = [board[1][1], board[0][1], board[1][0]];
+            }
+            if (x === 1 && y === 0) {
+              neighborsAre = [board[1][1], board[0][0], board[0][1], board[2][0], board[2][1]];
+            }
+            if (x === 2 && y === 0) {
+              neighborsAre = [board[1][1], board[1][0], board[2][1]];
+            }
+            if (x === 1 && y === 1) {
+              neighborsAre = [board[0][0], board[1][0], board[2][0], board[0][1], board[0][2], board[2][1], board[2][2], board[1][2]];
+            }
+            if (x === 1 && y === 2) {
+              neighborsAre = [board[1][1], board[0][1], board[0][2], board[2][1], board[2][2]];
+            }
+            if (x === 2 && y === 1) {
+              neighborsAre = [board[1][1], board[2][0], board[1][0], board[1][2], board[2][2]];
+            }
+            if (x === 2 & y === 2) {
+              neighborsAre = [board[1][1], board[2][1], board[1][2]];
+            }
+            if (x === 0 && y === 1) {
+              neighborsAre = [board[1][1], board[0][0], board[1][0], board[0][2], board[1][2]];
+            }
+            if (x === 0 && y === 2) {
+              neighborsAre = [board[1][1], board[0][1], board[1][2]];
+            }
+        }
 
+        //takes poistion/neighors and runs them through conway's rule set
+        var newState = false;
+        function conway(pos, neighborsAre){
+            var liveBros = 0;
+
+            for (var n = 0; n < neighbors.length; n++){
+              if (neighbors[n] === true){
+                liveBros++;
+              }
+              if (pos === false){
+                if (liveBros === 3){
+                  newState = true;
+                }
+                else {
+                  newState = false;
+                }
+              }
+              if (pos === true){
+                if (liveBros < 2){
+                  newState = false;
+                }
+                if (liveBros > 3){
+                  newState = false;
+                }
+                else{
+                    newState = true;
+                }
+              }
+             //adding them into a new array "newTick" (Doesn't seem to work yet!!)
+           }
+        }
     }
 }
-
-var neighbors;
-function neighborOf(board, x, y) {
-  if (x === 0 && y === 0) {
-      neighbors = [board[1][1], board[0][1], board[1][0]];
-  }
-  if (x === 1 && y === 0) {
-      neighbors = [board[1][1], board[0][0], board[0][1], board[2][0], board[2][1]];
-  }
-  if (x === 2 && y === 0) {
-      neighbors = [board[1][1], board[1][0], board[2][1]];
-  }
-  if (x === 1 && y === 1) {
-      neighbors = [board[0][0], board[1][0], board[2][0], board[0][1], board[0][2], board[2][1], board[2][2], board[1][2]];
-  }
-  if (x === 1 && y === 2) {
-      neighbors = [board[1][1], board[0][1], board[0][2], board[2][1], board[2][2]];
-  }
-  if (x === 2 && y === 1) {
-      neighbors = [board[1][1], board[2][0], board[1][0], board[1][2], board[2][2]];
-  }
-  if (x === 2 & y === 2) {
-      neighbors = [board[1][1], board[2][1], board[1][2]];
-  }
-  if (x === 0 && y === 1) {
-      neighbors = [board[1][1], board[0][0], board[1][0], board[0][2], board[1][2]];
-  }
-  if (x === 0 && y === 2) {
-      neighbors = [board[1][1], board[0][1], board[1][2]];
-  }
-  return neighbors;
-}
-
-var newState;
-function conway(pos, neighbors){
-  var liveBros = 0;
-  for (var i = 0; i < neighbors.length; i++){
-    if (neighbors[i] === true){
-       liveBros++;
-    }
-  }
-    if (pos === false){
-      if (liveBros === 3){
-        newState = true;
-      }
-      else {
-        newState = false;
-      }
-    }
-    if (pos === true){
-      if (liveBros < 2){
-        newState = false;
-      }
-      if (liveBros > 3){
-        newState = false;
-      }
-      else{
-        newState = true;
-      }
-  }
-
-   return newState;
-}
+//
+// once I figure out the single list for tick, can experiment returning a list of lists
+// console.log(newTick);
+console.log("Cell states are: " + cellBool);
+console.log("Cell coords are: "+ cellLoc);
+console.log("\nVictory, or Death!");
