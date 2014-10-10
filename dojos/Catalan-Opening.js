b02e4ff822be9246fb5cd259eba2c47f1e8f0fe3
Nicholas Hare <oharemarketing@gmail.com>
2014-10-10 12:28:55 -0400

    working with Catalan

--- dojos/Catalan-Opening.js
+++ dojos/Catalan-Opening.js
@@ -14,7 +14,7 @@ Just for me to visualize!
 
 */
 
-// var assert = require('assert');
+var assert = require('assert');
 
 // console.log(board.join('\n') + '\n\n');
 
@@ -28,37 +28,55 @@ Just for me to visualize!
 // board[4][2] = board[6][2];
 // board[6][2] = ' ';
 //
+// console.log(board.join('\n'));
+
 // // Black Move 1 (Pawn)
 // board[3][3] = board[1][3];
 // board[1][3] = ' ';
 //
+// console.log(board.join('\n'));
+
 // // White Move 2 (Pawn)
 // board[4][3] = board[6][3];
 // board[6][3] = ' ';
 //
+// console.log(board.join('\n'));
+
 // // Black Move 2 (Pawn)
 // board[2][4] = board[1][4];
 // board[1][4] = ' ';
 //
+// console.log(board.join('\n'));
+
 // // White Move 3 (Knight)
 // board[5][5] = board[7][6];
 // board[7][6] = ' ';
 //
+// console.log(board.join('\n'));
+
 // // Black Move 3 (Bishop)
 // board[1][4] = board[0][5];
 // board[0][5] = ' ';
 //
+// console.log(board.join('\n'));
+
 // // White Move 4 (Pawn)
 // board[5][6] = board[6][6];
 // board[6][6] = ' ';
 //
+// console.log(board.join('\n'));
+
 // // Black Move 4 (Knight)
 // board[2][5] = board[0][6];
 // board[0][6] = ' ';
 //
+// console.log(board.join('\n'));
+
 // // White Move 5 (Bishop)
 // board[6][6] = board[7][5];
 // board[7][5] = ' ';
+//
+// console.log(board.join('\n'));
 
 var board = [
   ['R','N','B','Q','K','B','N','R'],
@@ -69,6 +87,7 @@ var board = [
   [' ',' ',' ',' ',' ',' ',' ',' '],
   ['p','p','p','p','p','p','p','p'],
   ['r','n','b','q','k','b','n','r'] ];
+
 
 function catalan(board){
     var moves = [
@@ -88,8 +107,8 @@ function catalan(board){
           y = ' ';
         });
     });
-    return board.join('\n');
+    console.log(board.join('\n'));
 }
 
 
-console.log(catalan);
+catalan(board);
