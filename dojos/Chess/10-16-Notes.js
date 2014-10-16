991444d35fcc0e81cad964347923aed824cbe676
Nicholas Hare <oharemarketing@gmail.com>
2014-10-16 11:49:26 -0400

    notes at breal

--- dojos/Chess/10-16-Notes.js
+++ dojos/Chess/10-16-Notes.js
@@ -2,20 +2,49 @@
 
 var expect = require('chai').expect;
 
-var ralph = { name: "Ralph", gender: 'm'}
-var fabi = {name: "Fabi", gender: 'f'}
+var ralph = { name: 'Ralph', gender: 'm'}
+var fabi = {name: 'Fabi', gender: 'f'}
 var dave = {name: 'Dave', gender: 'm'}
 
 var pluck = function(collection, property){
+  var plucked = [];
   collection.forEach(function(element, index, array){
-    return element[property]
+      plucked.push(element[property]);
   })
+  return plucked;
+}
+
+var remove = function(collection, callback){
+  var removed = [];
+  collection.forEach(function(element, index, array){
+    if(callback(element) === true){
+      removed.push(element);
+    }
+  });
+  return removed;
 }
 
 describe ('he pluck function', function (){
-    it('should give back an array of strings when the prop
-    plucked is a string', function (){
+    it('should give back an array of strings when the prop plucked is a string', function (){
         var collection = [ ralph, fabi, dave]
-        expect(pluck(collection, 'gender')).to.eq(['m', 'f', 'm']);
+        expect(pluck(collection, 'gender')).to.deep.eq(['m', 'f', 'm']);
+        expect(pluck(collection, 'name')).to.deep.eq(['Ralph', 'Fabi', 'Dave']);
+    });
+});
+
+describe('the remove function', function(){
+    // it('returns an array of numbers for which a callback function is true', function(){
+    //     var collection = [ 1, 2, 3, 4]
+    //     var callback = function(n){
+    //       return true
+    //     expect(remove(collection, callback)).to.deep.eq([1, 2, 3, 4]);
+    //     }
+    // });
+    it('returns the even numbers of an array when the callback is true only for evens', function(){
+        var collection = [1, 2, 3, 4]
+        var callback = function(n){
+          return n % 2 === 0;
+        }
+        expect(remove(collection, callback)).to.deep.eq([2, 4]);
     });
 });
