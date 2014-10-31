3eb13753273b20b33fc9638d0cc6bcc04691a7bf
Nicholas Hare <oharemarketing@gmail.com>
2014-10-31 10:44:05 -0400

    asoper29 HTML PG / starting JS with phillycc

--- dojos/Responsive/Product-Grid/scripts.js
+++ dojos/Responsive/Product-Grid/scripts.js
@@ -1,1 +1,27 @@
 // product grid js
+
+/*
+
+add onClick event to images flip icon
+
+onClick event should give css transition class
+
+*/
+
+function flipIt (){
+
+}
+
+
+
+
+// var navItems = document.getElementsByTagName('h3');
+//       _.each(navItems, function(navItem){
+//         navItem.onclick = clickNavItem;
+//       });
+
+// function makeEverythingClosed(elements){
+//   _.each(elements, function(element){
+//     element.parentElement.className = '';
+//   });
+// }
