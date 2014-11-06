9d0b5f4f50712d29792ea9a3827e926f7b1b78e1
Nicholas Hare <oharemarketing@gmail.com>
2014-11-06 15:49:38 -0500

    Gulp file update

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,35 @@
 var gulp = require('gulp'),
-    connect = require('gulp-connect'),
-    args = require('yargs')
-        .alias('p', 'path')
-        .demand(['path'])
-        .alias('w', 'watch')
-        .argv;
+    connect = require('gulp-connect');
+    // args = require('yargs')
+    //     .alias('p', 'path')
+    //     .demand(['path'])
+    //     .alias('w', 'watch')
+    //     .argv;
+
+// gulp.task('default', [ ], function(){
+//
+// });
+
+
 
 gulp.task('connect', function(){
   connect.server({
-    root: args.path,
     livereload: true
   });
 });
 
 gulp.task('html', function(){
-  gulp.src(args.watch)
+  gulp.src('./dojos/**/*.*')
     .pipe(connect.reload());
 });
 
 gulp.task('watch', function(){
-  args.watch && gulp.watch([ args.watch ], [ 'html' ]);
+  gulp.watch(['./dojos/**/*.*'], ['reload']);
+  // args.watch && gulp.watch([ args.watch ], [ 'html' ]);
 });
 
-gulp.task('open', function(){
-  require('child_process').exec('open http://localhost:8080');
-});
+// gulp.task('open', function(){
+//   require('child_process').exec('open http://localhost:8080');
+// });
 
-gulp.task('default', ['connect', 'watch', 'open']);
+gulp.task('default', ['connect', 'watch']);
