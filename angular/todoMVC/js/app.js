a2b3343af8ea5713e5c7864e5a1757e37a9a90a8
Nicholas Hare <oharemarketing@gmail.com>
2014-11-18 14:32:10 -0500

    after lunch commit

--- angular/todoMVC/js/app.js
+++ angular/todoMVC/js/app.js
@@ -2,23 +2,67 @@
 	'use strict';
 
 	angular.module('todomvc', [ ])
-		.controller('Ctrl', function(){
-			this.tasks = [
-			 { text: 'Nicholas Patrick OHare'},
-			 { text: 'Isabella Von Carstein'},
-			];
+    .factory('TaskList', [ '$filter', '$http', function($filter, $http){
+      return self = {
+        tasks: [ ],
+        get: function(){
+          return $http.get("tasks.json")
+            .success(function(data){
+              self.tasks = data;
+            });
+        },
+        completed: function(){
+          return $filter('filter')(self.tasks, function(task){
+            return !task.completed;
+          });
+        },
+        addTask: function(task){
+          self.tasks.push({ text: task });
+        },
+        removeTask: function(task){
+        },
+        editTask: function(task, text){
+          task.text = text;
 
-		this.cancelEdit = function(keyCode, task){
-			if(keyCode === 27){
-					task.editing = false;
-			}
-		};
+          self.cancelEditing(task);
+        },
+        startEditing: function(task){
+          task.editing = true;
+        },
+        cancelEditing: function(task){
+          task.editing = false;
+        },
+      };
+    } ]) // END factory(TaskList)
 
-		this.startEdit = function(task){
-			task.editing = true;
-		};
-	})
-	;
-	// });
+    .controller('Ctrl', [ 'TaskList', function(TaskList){
+      console.log(TaskList);
+      var self = this,
+          tasks = this.tasks = [ ];
+
+      TaskList.get().success(function(){
+        tasks = self.tasks = TaskList.tasks;
+      });
+
+      this.completed = TaskList.completed;
+
+      this.startEdit = TaskList.startEditing;
+
+      this.editTask = TaskList.editTask;
+
+      this.cancelEdit = function($event, task){
+        if ( $event.keyCode === 27 ){
+          task.editing = false;
+        }
+      };
+
+      this.addTask = function($event, task){
+        if ( $event.keyCode === 13 ){
+          TaskList.addTask(task);
+        }
+      };
+
+    } ]) // END controller(Ctrl)
+  ; // END module(todomvc)
 
 })( window );
