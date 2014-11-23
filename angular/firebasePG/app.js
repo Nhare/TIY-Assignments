f498347da57b7a6b26b687de11d373ea3ac6a386
Nicholas Hare <oharemarketing@gmail.com>
2014-11-23 14:59:24 -0500

    firebase tutorials done

---
+++ angular/firebasePG/app.js
@@ -0,0 +1,47 @@
+// Firebase Playground
+
+var myApp = angular.module("myApp", ["firebase"]);
+
+      myApp.controller('MyController', ['$scope', '$firebase',
+        function($scope, $firebase) {
+          var ref = new Firebase("https://zsxedrfkba1.firebaseio-demo.com/");
+          $scope.messages = $firebase(ref).$asArray();
+
+          //ADD MESSAGE METHOD
+          $scope.addMessage = function(e) {
+
+            //LISTEN FOR RETURN KEY
+            if (e.keyCode === 13 && $scope.msg) {
+              //ALLOW CUSTOM OR ANONYMOUS USER NAMES
+              var name = $scope.name || 'anonymous';
+			  $scope.messages.$add({from: name, body: $scope.msg});
+              //RESET MESSAGE
+              $scope.msg = "";
+            }
+          };
+        }
+      ]);
+
+// app.controller("SampleCtrl", function($scope, $firebase) {
+//   var ref = new Firebase("https://intense-inferno-770.firebaseio.com/messages");
+//   var sync = $firebase(ref);
+//
+//   $scope.messages = sync.$asArray();
+//
+//   $scope.addMessage = function(text) {
+//     $scope.messages.$add({text: text});
+//   };
+
+//   app.controller("SampleCtrl", ["$scope", "$firebaseAuth",
+//   function($scope, $firebaseAuth) {
+//     var ref = new Firebase("https://intense-inferno-770.firebaseio.com/");
+//     var auth = $firebaseAuth(ref);
+//     auth.$authWithOAuthPopup("facebook").then(function(authData) {
+//       console.log("Logged in as:", authData.uid);
+//     }).catch(function(error) {
+//       console.error("Authentication failed: ", error);
+//     });
+//   }
+// ]);
+
+// });
